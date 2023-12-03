a2d23e9e9ee0c8d2c28f9bffd76a940aa3800244
Jose Pablo Fallas Diaz <3027031+pfadiaz@users.noreply.github.com>
2023-12-03 22:16:56 +0000

    Revert "ading mono repo (#14)"
    This reverts commit 03592fa188ac38a8c26c8fcf484245ab39303227.

---
+++ test/pages/index.js
@@ -0,0 +1,15 @@
+import loginPage from './login.page.js';
+import inventoryPage from './inventory.page.js';
+import cartPage from './cart.page.js';
+import checkoutStepOnePage from './checkout/checkout-step-one.page.js';
+import checkoutStepTwoPage from './checkout/checkout-step-two.page.js';
+import checkoutStepCompletePage from './checkout/checkout-complete.page.js';
+
+export default {
+  loginPage,
+  inventoryPage,
+  cartPage,
+  checkoutStepOnePage,
+  checkoutStepTwoPage,
+  checkoutStepCompletePage,
+};
